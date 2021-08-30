b5562477fd12238d127e0bb45bfa88f7f099da44
Ignat Moseykov <Moseyjr99@gmail.com>
2021-08-30 20:17:56 +0700

    testing groups permissions

--- control/views.py
+++ control/views.py
@@ -132,12 +132,13 @@ def update_info_about_variables(request):
         elif is_oper(request.user):
             user = request.user.username
             user_id = ScUsers.objects.get(name=user)
-            print(user_id)
-            list_of_users = [user_id]
+            print(user_id.id)
+            list_of_users = [user_id.id]
             group = models.Group.objects.get(name='Opers')
             query_users = group.user_set.all()
             for g in query_users:
-                list_of_users.append(ScUsers.objects.get(name=g))
+                user_id_from_group = ScUsers.objects.get(name=g)
+                list_of_users.append(user_id_from_group.id)
             print(list_of_users)
 
             data = serialize("json", ScPaths.objects.filter(user_id=user_id.id))

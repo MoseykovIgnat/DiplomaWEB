032022bb272a462ad34ec438eb2a702ee5fc3b7c
Ignat Moseykov <Moseyjr99@gmail.com>
2021-08-30 20:39:21 +0700

    testing groups permissions

--- control/views.py
+++ control/views.py
@@ -141,7 +141,8 @@ def update_info_about_variables(request):
                 user_id_from_group = ScUsers.objects.get(name=g)
                 list_of_users.append(user_id_from_group.id)
             print(list_of_users)
-            data = serialize("json", ScPaths.objects.filter(user_id__in=list_of_users))
+            # data = serialize("json", ScPaths.objects.filter(user_id__in=list_of_users))
+            data = serialize("json", ScPaths.objects.filter(user_id__in=user_id))
         return HttpResponse(data, content_type='application/json')
 
 

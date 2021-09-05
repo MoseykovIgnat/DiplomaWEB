436d60c66e1ab76e6222374fb57e58e371d9a904
Ignat Moseykov <Moseyjr99@gmail.com>
2021-09-06 04:16:32 +0700

    Working on "Edit graph-editor to save name of graph"

--- control/admin.py
+++ control/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 from .models import ScUsers, ScPaths, ScResults, ScConditions, ScConditionsResult, ScGraphName, ScGraphInfo
-
 admin.site.register(ScPaths)
 admin.site.register(ScUsers)
 admin.site.register(ScConditions)
 admin.site.register(ScConditionsResult)
-
+admin.site.register(ScGraphName)
+admin.site.register(ScGraphInfo)
 
 class ScResultsAdmin(admin.ModelAdmin):
     list_display = ('var_title', 'value', 'flag', 'submission_date', 'comment')
     list_filter = ('value', 'submission_date')
 
 
-admin.site.register(ScResults, ScResultsAdmin, ScGraphName, ScGraphInfo)
+admin.site.register(ScResults, ScResultsAdmin)
 # Register your models here.
+

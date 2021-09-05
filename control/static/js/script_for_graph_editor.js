fd429729c32d22bbf3293dd20cb0f023047dce9a
Ignat Moseykov <Moseyjr99@gmail.com>
2021-09-06 04:00:39 +0700

    Working on "Edit graph-editor to save name of graph"

--- control/static/js/script_for_graph_editor.js
+++ control/static/js/script_for_graph_editor.js
@@ -622,7 +622,7 @@ document.onload = (function(d3, saveAs, Blob, undefined) {
 
   /** MAIN SVG **/
   var svg = d3.select(".svg_place").append("svg")
-    .attr('class','mysvg');
+    .attr('id','mysvg');
 
   var graph = new GraphCreator(svg, nodes, edges);
   graph.setIdCt(2);

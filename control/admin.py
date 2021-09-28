614d92cb79fb43c0f646a62679aeb95efbfdc46f
Ignat Moseykov <Moseyjr99@gmail.com>
2021-09-28 13:13:38 +0700

    Fixing error with save a new variable from condition

--- control/admin.py
+++ control/admin.py
@@ -1,17 +1,50 @@
 from django.contrib import admin
 from .models import ScUsers, ScPaths, ScResults, ScConditions, ScConditionsResult, ScGraphName, ScGraphInfo
-admin.site.register(ScPaths)
-admin.site.register(ScUsers)
-admin.site.register(ScConditions)
-admin.site.register(ScConditionsResult)
-admin.site.register(ScGraphName)
-admin.site.register(ScGraphInfo)
+
+
+class ScConditionsResultAdmin(admin.ModelAdmin):
+    list_display = ('cond_id', 'val_result', 'bool_result', 'val_formula', 'text_formula', 'result_formula',
+                    'time_calc', 'empty_values')
+    list_filter = ('dot_name', 'dot_condition')
+
+
+class ScConditionsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'formula', 'max_val', 'min_val', 'limit_val', 'comment', 'display_method', 'priority',
+                    'isalert', 'alert_interval', 'time_create_or_alert')
+    list_filter = ('user', 'formula')
+
+
+class ScGraphInfoAdmin(admin.ModelAdmin):
+    list_display = ('dot_name', 'dot_condition', 'dot_id_in_graph', 'graph')
+    list_filter = ('dot_name', 'dot_condition')
+
+
+class ScGraphNameAdmin(admin.ModelAdmin):
+    list_display = 'graph_name'
+    list_filter = 'graph_name'
+
+
+class ScPathsAdmin(admin.ModelAdmin):
+    list_display = ('path', 'interval_time', 'status', 'user')
+    list_filter = ('path', 'interval_time')
+
 
 class ScResultsAdmin(admin.ModelAdmin):
     list_display = ('var_title', 'value', 'flag', 'submission_date', 'comment')
     list_filter = ('value', 'submission_date')
 
 
-admin.site.register(ScResults, ScResultsAdmin)
-# Register your models here.
+class ScUsersAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status')
+    list_filter = ('name', 'status')
 
+
+admin.site.register(ScUsers, ScUsersAdmin)
+admin.site.register(ScPaths, ScPathsAdmin)
+admin.site.register(ScResults, ScResultsAdmin)
+admin.site.register(ScConditionsResult, ScConditionsResultAdmin)
+admin.site.register(ScConditions, ScConditionsAdmin)
+admin.site.register(ScGraphInfo, ScGraphInfoAdmin)
+admin.site.register(ScGraphName, ScGraphNameAdmin)
+
+# Register your models here.

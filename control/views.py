315b8f1181730ef4c2eccb4b97e17b7f49ee7c67
Ignat Moseykov <Moseyjr99@gmail.com>
2021-09-28 12:29:32 +0700

    Fixing error with save a new variable from condition

--- control/views.py
+++ control/views.py
@@ -70,6 +70,7 @@ def update_condition_results():
         vars_formula = get_vars_formula(formula)
         empty_values = list()
         for var_formula in vars_formula:
+            print(str(ScResults.objects.filter(var_title=var_formula)[0].value))
             formula = formula.replace(str(var_formula),
                                       str(ScResults.objects.filter(var_title=var_formula)[0].value))
             if str(ScResults.objects.filter(var_title=var_formula)[0].comment) == 'Empty':

d4c2b8a5857618ca7da94f22fa7af6b1ddc3b36b
Ignat Moseykov <Moseyjr99@gmail.com>
2021-10-22 17:45:04 +0700

    Test custom auth

--- WEB/backends.py
+++ WEB/backends.py
@@ -27,19 +27,17 @@ class PersonalizedLoginBackend(ModelBackend):
             print('User already exist')
             return None
         except:
-            try:
-                print("Let's create a user")
-                config = SQLParser.xxxdbrc.config('adm')
-                connection = create_connect_to_db(config)
-                cursor = connection.cursor(MySQLdb.cursors.DictCursor)
-                cursor.execute("select c_passwd from t_shadow where c_account=%s" % username)
-                cryptedpasswd = cursor.fetchall()
-                print(cryptedpasswd)
-                # user = User.objects.create_user(username=username, password=password)
-                # user.save()
-                # print('User was created')
-            finally:
-                return None
+            print("Let's create a user")
+            config = SQLParser.xxxdbrc.config('adm')
+            connection = create_connect_to_db(config)
+            cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute("select c_passwd from t_shadow where c_account=%s" % username)
+            cryptedpasswd = cursor.fetchall()
+            print(cryptedpasswd)
+            # user = User.objects.create_user(username=username, password=password)
+            # user.save()
+            # print('User was created')
+            return None
             # try:
 
             # except:

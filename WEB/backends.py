19cadf2ac53c7c76197cc624a396e04ed095406e
Ignat Moseykov <Moseyjr99@gmail.com>
2021-10-22 18:01:40 +0700

    Test custom auth

--- WEB/backends.py
+++ WEB/backends.py
@@ -8,6 +8,7 @@ import pymysql as MySQLdb
 import crypt
 from hmac import compare_digest as compare_hash
 
+
 def create_connect_to_db(config):
     connection = MySQLdb.connect(host=config["hostname"],
                                  user=config["username"],
@@ -18,42 +19,31 @@ def create_connect_to_db(config):
 
     return connection
 
+
 UserModel = get_user_model()
-### select c_passwd from t_shadow where c_account='imoseikov';
+
+
 class PersonalizedLoginBackend(ModelBackend):
     def authenticate(self, request=None, username=None, password=None, **kwars):
         try:
             UserModel._default_manager.get_by_natural_key(username)
             print('User already exist')
-            print("Let's create a user")
-            config = SQLParser.xxxdbrc.config('adm')
-            connection = create_connect_to_db(config)
-            cursor = connection.cursor(MySQLdb.cursors.DictCursor)
-            query_for_crypted_password = "select c_passwd from t_shadow where c_account=%s"
-            cursor.execute(query_for_crypted_password, username)
-            crypted_password = cursor.fetchone()['c_passwd']
-            print(crypted_password)
-            print(compare_hash(crypt.crypt(password, crypted_password), crypted_password))
             return None
         except:
-            # print("Let's create a user")
-            # config = SQLParser.xxxdbrc.config('adm')
-            # connection = create_connect_to_db(config)
-            # cursor = connection.cursor(MySQLdb.cursors.DictCursor)
-            # query_for_crypted_password = "select c_passwd from t_shadow where c_account=%s"
-            # cursor.execute(query_for_crypted_password, username)
-            # crypted_password = cursor.fetchone()['c_passwd']
-            # print(crypted_password)
-            # print(compare_hash(crypt.crypt(password, crypted_password), crypted_password))
-            # user = User.objects.create_user(username=username, password=password)
-            # user.save()
-            # print('User was created')
-            return None
-            # try:
-
-            # except:
-            #     print('WTF')
-
-
-
-
+            try:
+                print("Let's create a user")
+                config = SQLParser.xxxdbrc.config('adm')
+                connection = create_connect_to_db(config)
+                cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+                query_for_crypted_password = "select c_passwd from t_shadow where c_account=%s"
+                cursor.execute(query_for_crypted_password, username)
+                crypted_password = cursor.fetchone()['c_passwd']
+                cursor.close()
+                connection.close()
+                print(crypted_password)
+                if compare_hash(crypt.crypt(password, crypted_password), crypted_password):
+                    user = User.objects.create_user(username=username, password=password)
+                    user.save()
+                    print('User was created')
+            finally:
+                return None

11ca6d0b6e7dddecf36f1c37386fe1b83221153b
Ignat Moseykov <Moseyjr99@gmail.com>
2021-10-22 16:13:02 +0700

    Test custom auth

--- WEB/backends.py
+++ WEB/backends.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.hashers import *
+from django.contrib.auth import get_user_model
+UserModel = get_user_model()
 
 
 class PersonalizedLoginBackend(ModelBackend):
     def authenticate(self, request=None, username=None, password=None, **kwars):
-        print('HI')
+        if UserModel._default_manager.get_by_natural_key(username):
+            print('HI')
         return None

94581521f2c9431631ae3cfc5704d24765dbe867
Ignat Moseykov <Moseyjr99@gmail.com>
2021-11-18 23:46:59 +0700

    Auto completion

--- control/views.py
+++ control/views.py
@@ -193,12 +193,11 @@ def del_exist_condition(request):
 
 def search_info_for_autocomplete(request):
     if request.method == 'GET' and request.is_ajax():
-        print(request.GET.get('lastItem'))
         data = []
         print(request.GET.get('nameStartsWith'))
-        limit = int(request.GET.get('maxRows'))
+        limit = request.GET.get('maxRows')
         #Фильтр либо в комментарии, либо в имени
-        qs = ScVariableAutoCompletion.objects.filter(name__in=request.GET.get('nameStartsWith')).order_by('name')[:10]
+        qs = ScVariableAutoCompletion.objects.filter(name__in=request.GET.get('nameStartsWith')).order_by('name')[:limit]
         for name in qs:
             data.append(name.name)
             print(name.name)

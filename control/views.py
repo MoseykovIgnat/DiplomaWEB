8a99c31bf557f29616e1229cf974467fcf843f9b
Ignat Moseykov <Moseyjr99@gmail.com>
2022-04-02 17:07:04 +0700

    get new alert sound view

--- control/views.py
+++ control/views.py
@@ -104,10 +104,10 @@ def get_new_alert_sound(request):
         most_primary_alert = {"id": 0, "priority": 0}
         for element in new_alerts_to_play:
             print(element["alert_id"])
-            alert = ScAlertHistory.objects.get(id=element['alert_id']).values()
-            result["alerts"].append(alert)
-            if alert.priority > most_primary_alert["priority"]:
-                most_primary_alert["id"] = alert.id
+            # alert = ScAlertHistory.objects.get(id=element['alert_id']).values()
+            # result["alerts"].append(alert)
+            # if alert.priority > most_primary_alert["priority"]:
+            #     most_primary_alert["id"] = alert.id
 
         print(result)
         return HttpResponse(new_alerts_to_play, content_type='application/json')

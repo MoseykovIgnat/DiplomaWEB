e9df6063d7352ebfbad9082c849d5b0158374e37
Ignat Moseykov <Moseyjr99@gmail.com>
2022-04-03 01:13:08 +0700

    music test

--- control/views.py
+++ control/views.py
@@ -116,7 +116,7 @@ def get_new_alert_sound(request):
                 if alert["priority"] > most_primary_alert["priority"]:
                     most_primary_alert["id"] = alert["id"]
         if most_primary_alert["id"] != 0:
-            result["most_primary_alert"] = model_to_dict(ScAlertHistory.objects.get(id=most_primary_alert["id"]))
+            result["most_primary_alert"] = ScAlertHistory.objects.get(id=most_primary_alert["id"])
         print(result)
         ScAlertSoundPlayer.objects.filter(Q(alert_id__in=ids_of_new_alerts_to_play) & Q(user_id=user_id)).delete()
         return HttpResponse(json.dumps(result, default=str), content_type='application/json')

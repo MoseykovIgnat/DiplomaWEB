552c0a597fbd8d2aa8377ec03e81b18d95c0c085
Ignat Moseykov <Moseyjr99@gmail.com>
2022-04-03 20:48:29 +0700

    improve history alert page

--- control/views.py
+++ control/views.py
@@ -92,7 +92,7 @@ def load_alert_data(request) -> render:
     # your_conditions = ScAlertHistory.objects.filter(creator=user, is_required_condition=0).order_by('-time_calc')
     # required_conditions = ScAlertHistory.objects.filter(is_required_condition=1).order_by('-time_calc')
     all_conditions = ScAlertHistory.objects.filter(
-        (Q(creator=user) & Q(is_required_condition=0)) | Q(is_required_condition=1)).order_by('-time_calc')[:100].values()
+        (Q(creator=user) & Q(is_required_condition=0)) | Q(is_required_condition=1)).order_by('-time_calc')[:100]
     return render(
         request,
         'alert.html',
@@ -101,7 +101,12 @@ def load_alert_data(request) -> render:
 
 
 def get_more_alert_history_info(request):
-    pass
+    if request.method == 'GET' and request.is_ajax():
+        user = request.user.username
+        new = ScAlertHistory.objects.filter(
+            ((Q(creator=user) & Q(is_required_condition=0)) | Q(is_required_condition=1)) & Q(id__gt=request.POST.get('last_alert_id'))).order_by('-time_calc').values()
+        print(new)
+        return HttpResponse(json.dumps(new, default=str), content_type='application/json')
 
 
 def get_new_alert_sound(request):
